fb3ce8b89774267634e68ec28500d39d189e1694
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2021-09-27 15:24:19 +0600

    server created

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in  on port ${PORT}`
+  )
+)
