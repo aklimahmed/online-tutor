3333ee5cada084d52c291a57e68eeb851fb95d5e
Rashadul Islam <rashadulislam4581@gmail.com>
2021-09-27 18:59:36 +0600

    new

--- frontend/src/components/common/Header/Header.js
+++ frontend/src/components/common/Header/Header.js
@@ -1,36 +1,50 @@
 import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import "./Header.scss";
+import logo from "../../../Asset/Picture/logo.png";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 
-const Header = () => {
+const header = () => {
   return (
-    <div>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
+    <div className="header">
+      <Navbar className="container" expand="lg">
+        <Container fluid>
+          <img className="header-logo" src={logo} alt="" />
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0 navStyle"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
             >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
+              <NavDropdown title="CLASSES" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="SUBJECTS" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link className="nav-title" href="#action1">TUTOR</Nav.Link>
+              <Nav.Link className="nav-title" href="#action2">STUDENTS</Nav.Link>
+              <Nav.Link className="nav-title" href="#action2">PARENTS</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
 
-export default Header;
+export default header;
