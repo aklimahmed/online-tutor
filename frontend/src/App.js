01c5e7adaa734c08bc3f47d9af3ac196df14fa67
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2021-09-28 14:30:54 +0600

    app.js file fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
@@ -21,17 +20,7 @@ const App = () => {
      
     </Router>
   )
-=======
-import React from "react";
-import Header from "./components/common/Header/Header";
 
-function App() {
-  return (
-    <div className="App">
-     <Header/>
-    </div>
-  );
->>>>>>> 38847d6a1b21b4596a16ec9ae52c3ca33759209b
 }
 
 export default App
