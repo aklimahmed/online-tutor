1b6787d5712f8d423f1ce58b9adeee07d6c0cc1f
Rashadul Islam <rashadulislam4581@gmail.com>
2021-10-04 18:13:29 +0600

    multi-select solved

--- frontend/src/components/TutorsProfile/SubJectTeaches.js
+++ frontend/src/components/TutorsProfile/SubJectTeaches.js
@@ -1,9 +1,37 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import { MultiSelect } from "react-multi-select-component";
 import { Row, Col } from "react-bootstrap";
 
-const subject = [{ sub: "Physics" }, { sub: "Math" }, { sub: "English" }];
-
 const SubJectTeaches = () => {
+  const subject = [{ sub: "Physics" }, { sub: "Math" }, { sub: "English" }];
+  const options = [
+    { value: "Physics", label: "Physics" },
+    { value: "Math", label: "Math" },
+    { value: "English", label: "English" },
+  ];
+
+  const [selected, setSelected] = useState([]);
+  const [input, setinput] = useState({
+    primarySubject: "",
+    secondarySubjects: [],
+  });
+
+  useEffect(() => {
+    input.secondarySubjects=[]
+    const hold = [...selected];
+    hold.map((data) => {
+      input.secondarySubjects.push(data.value);
+    });
+  }, [selected]);
+
+  const handleChangeInput = (event) => {
+    const usereInputs = { ...input };
+    usereInputs[event.target.name] = event.target.value;
+    setinput(usereInputs);
+  };
+
+  console.log(input);
+
   return (
     <div className="weeklyTime_div">
       <h6 className="time_heading">Subject Teaches:</h6>
@@ -12,7 +40,12 @@ const SubJectTeaches = () => {
           <label for="Select" className="form-label">
             Primary Subject(One subject):
           </label>
-          <select id="Select" className="form-select">
+          <select
+            id="Select"
+            className="form-select"
+            name="primarySubject"
+            onBlur={handleChangeInput}
+          >
             <option style={{ display: "none" }}>
               Select primary subject of teaching
             </option>
@@ -25,14 +58,12 @@ const SubJectTeaches = () => {
           <label for="Select" className="form-label">
             Secondary Subject(Multiple subjects):
           </label>
-          <select id="Select" className="form-select">
-            <option style={{ display: "none" }}>
-              Select secondary subject of teaching
-            </option>
-            {subject.map((sub) => (
-              <option value={sub.sub}>{sub.sub}</option>
-            ))}
-          </select>
+          <MultiSelect
+            options={options}
+            value={selected}
+            onChange={setSelected}
+            labelledBy="Select"
+          />
         </Col>
       </Row>
     </div>
