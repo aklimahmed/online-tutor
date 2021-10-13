ceb934ea808446b6dfb8909f942fc468f929a8bb
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2021-10-13 13:12:28 +0600

    userRoutes file fixed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,10 +6,6 @@ import {
 } from '../controllers/userController.js'
 
 router.route('/').post(registerUser)
-<<<<<<< HEAD
-=======
-
->>>>>>> ee481aaa853f0c62ded557811457d6814e703780
 router.post('/login', authUser)
 
 export default router;
